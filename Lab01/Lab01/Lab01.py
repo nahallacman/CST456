e414f52c0db0128841840e48547c84060acc30fc
Cal <nahallacman@gmail.com>
2015-01-16 01:47:42 -0800

    Lab 01 correct and complete
    uses command line arguments appropriately

--- Lab01/Lab01/Lab01.py
+++ Lab01/Lab01/Lab01.py
@@ -1,14 +1,23 @@
+import sys
+print("source, input, and output files:")
+for x in range(0, 3):
+    print (sys.argv[x])
 
-file_object = open('C:\\Users\\cal\\Desktop\\School\\CST456\\CST456-Week1\\example_input.s19', 'r')
+print("resulting file contents:")
+file_object = open(sys.argv[1], 'r')
+write_file = open(sys.argv[2], 'w')
 for line in file_object:
-    #print (line)
+
     lineseg1 = line[:2] # get the first 2 characters
     lineseg2 = line[2:4] # get characters 3 and 4
-    temp = int(lineseg2, 16)
-    line2 = line[4:]
+    temp = int(lineseg2, 16) #convert from base 16 to base 10 
+    line2 = line[4:] # get the first 4 characters
+
+    #declare variables
     len = 0
     line3 = ''
 
+    #collect the address, address length is based upon S#, S1 = 2 bytes, S2 = 3 bytes, S3 = 4 bytes
     if lineseg1 == "S1":
         len = 2
     if lineseg1 == "S2":
@@ -16,56 +25,25 @@ for line in file_object:
     if lineseg1 == "S3":
         len = 4
 
-    #print("address beginning: ")
     for y in range(0, len*2):
-        #print(line2[y])
         line3 += line2[y]
 
-    ram_start = int(line3, 16)
-    #print (ram_start)
+    ram_start = int(line3, 16) #convert from base 16 to base 10 
 
+    #more variables
     full_line = ''
     test = ''
+    ram_val = ram_start
+
+    #construct lines then print
     for x in range(len, temp-1):
         full_line += "ram["
-        full_line += str(ram_start)
+        full_line += str(ram_val)
         full_line += "] = 8'h"
-        full_line += str(line2[x*2:(x+1)*2]) # this line needs work
+        full_line += str(line2[x*2:(x+1)*2]) 
         full_line += ";"
         print (full_line)
+        write_file.write( full_line )
+        write_file.write( "\n" )
         full_line = ''
-        ram_start += 1
-
-        #print ("ram[") 
-        #print (ram_start)
-        #print ("] = 8'h")
-        #print(line2[x*2:(x+1)*2])
-        #print (";")
-
-
-#    if lineseg1 == 'S1':
- #       testing = 1
-  #      lineseg3 = line[4:8] # get characters 5 6 7 8
-   #     line2 = line[8:] # get all characters after 8
-    #if lineseg1 == 'S2':
-#        testing = 2
- #       lineseg3 = line[4:10] # get characters 5 6 7 8 9 10
-  #      line2 = line[10:] # get all characters after 8
-   # if lineseg1 == 'S3':
-    #    testing = 3
-     #   lineseg3 = line[4:12] # get characters 5 6 7 8 9 10 11 12
-      #  line2 = line[12:] # get all characters after 8
-#    print (lineseg1)
- #   print (lineseg2)
-  #  print (lineseg3)
-    
-   # for x in range(0, int(lineseg2)):
-    #    print (line2[x*2:(x+1)*2])
-
-
-
-#line = file_object.readline()
-#print (line)
-
-
-print('Hello World')
+        ram_val += 1
