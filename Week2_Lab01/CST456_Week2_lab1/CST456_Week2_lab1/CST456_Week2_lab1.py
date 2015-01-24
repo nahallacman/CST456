97bb598c13b37a3b69bbadfd07cef71254092199
Cal <nahallacman@gmail.com>
2015-01-24 04:53:06 -0800

    Script generation complete

--- Week2_Lab01/CST456_Week2_lab1/CST456_Week2_lab1/CST456_Week2_lab1.py
+++ Week2_Lab01/CST456_Week2_lab1/CST456_Week2_lab1/CST456_Week2_lab1.py
@@ -1,9 +1,9 @@
 
 
 #program header
-print("PROG_START	EQU		$2000\n   org		PROG_START	");
+print("PROG_START\tEQU\t\t$2000\n\t\t\torg\t\tPROG_START");
 
-#generate 256 address and value pairs
+#generate 256 address and value pairs for LDAA and STAA
 for x in range(0,256):
     temp = ''
     #construct a 2 digit hex number that has a leading 0 if the value is less than 0x10 (16 hex)
@@ -14,14 +14,36 @@ for x in range(0,256):
     else:
         temp3 = temp2;
     
-    #print the constucted string
+    #print the constucted string for testing coma
     temp4 = "#$" + temp3;
-    print("ldaa", temp4);
+    print("\t\t\tldaa", temp4);
 
-    print("coma");
+    #print("\t\t\tcoma");
 
     temp5 = "$40" + temp3;
-    print("staa", temp5);
+    print("\t\t\tstaa", temp5);
+
+#generate 256 address and value pairs for COMA
+for x in range(0,256):
+    #temp = ''
+    #construct a 2 digit hex number that has a leading 0 if the value is less than 0x10 (16 hex)
+    temp = hex(x);
+    temp2 = temp[2:];
+    if int(temp2, 16) < 0x10:
+        temp3 = "0" + temp2;
+    else:
+        temp3 = temp2;
+    
+    #print the constucted string for testing coma
+    temp4 = "#$" + temp3;
+    print("\t\t\tldaa", temp4);
+
+    print("\t\t\tcoma");
+
+    temp5 = "$41" + temp3;
+    print("\t\t\tstaa", temp5);
+
+
 
 #program footer
-print("jump		jmp     jump\n  org		$FFFE\n FDB		PROG_START");
+print("jump\t\tjmp\tjump\n\t\t\torg\t\t$FFFE\n\t\t\tFDB\t\tPROG_START");
