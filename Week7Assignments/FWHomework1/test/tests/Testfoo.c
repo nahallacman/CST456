02903387341a458657d2cb76c718c9226bf2061a
Cal Barkman <nahallacman@gmail.com>
2015-02-26 22:50:51 -0800

    Did work on week 7 tests
    Also added the pdf of the book for the second half of the class

--- Week7Assignments/FWHomework1/test/tests/Testfoo.c
+++ Week7Assignments/FWHomework1/test/tests/Testfoo.c
@@ -12,34 +12,54 @@ void tearDown(void)
 // Create one passing and one failing assertion using TEST_ASSERT_TRUE.
 void testTEST_ASSERT_TRUE (void)
 {
-
+	TEST_ASSERT_EQUAL(1, 0x01);
+	TEST_ASSERT_NOT_EQUAL(0, 0x01);
 }
 
 // Create one passing and one failing assertion using TEST_ASSERT_FALSE.
 void testTEST_ASSERT_FALSE (void)
 {
-
+	TEST_ASSERT_NOT_EQUAL(0, 1);
+	TEST_ASSERT_EQUAL( 1, 1);
 }
 
 // Create one passing and one failing assertion using TEST_ASSERT_EQUAL_INT8.
 // Try it with negative decimal numbers, positive decimal numbers, and hexadecimal numbers.
 void testTEST_ASSERT_EQUAL_INT8 (void)
 {
-
+	TEST_ASSERT_EQUAL_INT8(0x80, -128);
+	TEST_ASSERT_EQUAL_INT8(0xFF, -1);
+	TEST_ASSERT_EQUAL_INT8(0x01, 1);
+	TEST_ASSERT_EQUAL_INT8(1, 0x01);
+	TEST_ASSERT_EQUAL_INT8(1, 257);
+	TEST_ASSERT_EQUAL_INT8(0, 256);
+	TEST_ASSERT_EQUAL_INT8(-1, 255);
 }
 
 // Create one passing and one failing assertion using TEST_ASSERT_EQUAL_UINT8.
 // Try it with negative decimal numbers, positive decimal numbers, and hexadecimal numbers.
 void testTEST_ASSERT_EQUAL_UINT8 (void)
 {
-
+	TEST_ASSERT_EQUAL_UINT8(0x80, -128);
+	TEST_ASSERT_EQUAL_UINT8(0xFF, -1);
+	TEST_ASSERT_EQUAL_UINT8(0x01, 1);
+	TEST_ASSERT_EQUAL_UINT8(1, 0x01);
+	TEST_ASSERT_EQUAL_UINT8(1, 257);
+	TEST_ASSERT_EQUAL_UINT8(0, 256);
+	TEST_ASSERT_EQUAL_UINT8(-1, 255);
 }
 
 // Create one passing and one failing assertion using TEST_ASSERT_EQUAL_HEX8.
 // Try it with negative decimal numbers, positive decimal numbers, and hexadecimal numbers.
 void testTEST_ASSERT_EQUAL_HEX8 (void)
 {
-
+	TEST_ASSERT_EQUAL_HEX8(0x80, -128);
+	TEST_ASSERT_EQUAL_HEX8(0xFF, -1);
+	TEST_ASSERT_EQUAL_HEX8(0x01, 1);
+	TEST_ASSERT_EQUAL_HEX8(1, 0x01);
+	TEST_ASSERT_EQUAL_HEX8(1, 257);
+	TEST_ASSERT_EQUAL_HEX8(0, 256);
+	TEST_ASSERT_EQUAL_HEX8(-1, 255);
 }
 
 // Create one passing and one failing assertion using TEST_ASSERT_UINT_WITHIN.
@@ -70,5 +90,13 @@ void testTEST_ASSERT_BIT_HIGH (void)
 // squareNumber takes an 8-bit unsigned integer as input, squares it, and then returns the squared value as a 16-bit unsigned integer.
 void testsquareNumber (void)
 {
-	
+	uint8_t testval;
+	uint16_t count;
+	uint16_t returnval;
+	for( testval = 0, count = 0; count < 256; testval++, count++)
+	{
+		returnval = squareNumber( testval );
+		TEST_ASSERT_EQUAL(returnval, testval * testval);
+		// assert_equal(returnval, testval * testval);
+	}
 }
